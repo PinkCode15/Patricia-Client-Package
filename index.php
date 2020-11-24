f44b91116acd3a1934c479e995392108313c5b20
octacondeveloper <kaakadev@gmail.com>
2020-11-25 00:44:02 +0100

    wip

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ $attributes = " id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     lastname VARCHAR(30) NOT NULL,
     email VARCHAR(50),
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
-// $gh =  (new DatabaseManager)->upTable("new_users", $attributes);
+$gh =  (new DatabaseManager)->upTable("new_users", $attributes);
 
-$hh =  (new DatabaseManager)->downTable("new_users");
+// $hh =  (new DatabaseManager)->downTable("new_users");
 
-// var_dump($gh);
+var_dump($gh);
 
-var_dump($hh);
+// var_dump($hh);
  
