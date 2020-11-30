76ff3d0ee75c0d2e1e9b2717e6a6ffd27780ae7d
StaceyCreates <stacey@patricia.com.ng>
2020-11-30 12:50:11 +0100

    added client helpers and tests

--- tests/unit/ClientHelpersTest.php
+++ tests/unit/ClientHelpersTest.php
@@ -2,38 +2,36 @@
 use PHPUnit\Framework\TestCase;
 use PatriciaClient\Helpers\ClientHelpers;
 
-class MigrationTest extends TestCase 
+class ClientHelpersTest extends TestCase 
 {
     public function testCanCheckIsAdmin()
     {
-        $table_name = "test_table";
         $clientData = [
             "uuid" =>  uniqid('client-',true),
-            "name" => "TestAdmin",
+            "name" => "TestAdmin1",
             "type" => "admin",
             "is_blocked" => "0",
         ];
 
-        $clientLastID = (new dbConnection())->insertIntoTable("clients", $clientData);
-        $client = (new dbConnection())->selectById($table, $lastID);
+        $lastID = (new dbConnection())->insertIntoTable("clients", $clientData);
+        $client = (new dbConnection())->selectById("clients", $lastID);
 
         $clientKeysData = [
             "name" =>  "TestAdmin1",
-            "client_id" => $client->id,
+            "client_id" => $client['id'],
             "client_key" => 'pat_privkey_748jkvk099hjd',
             "is_blocked" => "0",
         ];
 
-        (new dbConnection())->insertIntoTable("client_keys", $data);
+        (new dbConnection())->insertIntoTable("client_keys", $clientKeysData);
 
-        $result = (new ClientHelpers())->isAdmin($clientKeysData->client_key);
+        $result = (new ClientHelpers())->isAdmin($clientKeysData['client_key']);
         $this->assertTrue(true);
  
     }
 
     public function testCanCheckIsAuthenticated()
     {
-        $table_name = "test_table";
         $clientData = [
             "uuid" =>  uniqid('client-',true),
             "name" => "TestAdmin2",
@@ -41,19 +39,19 @@ class MigrationTest extends TestCase
             "is_blocked" => "0",
         ];
 
-        $clientLastID = (new dbConnection())->insertIntoTable("clients", $clientData);
-        $client = (new dbConnection())->selectById($table, $lastID);
+        $lastID = (new dbConnection())->insertIntoTable("clients", $clientData);
+        $client = (new dbConnection())->selectById("clients", $lastID);
 
         $clientKeysData = [
             "name" =>  "TestAdmin2",
-            "client_id" => $client->id,
+            "client_id" => $client['id'],
             "client_key" => 'pat_privkey_mdlkfl7890',
             "is_blocked" => "0",
         ];
 
-        (new dbConnection())->insertIntoTable("client_keys", $data);
+        (new dbConnection())->insertIntoTable("client_keys", $clientKeysData);
 
-        $result = (new ClientHelpers())->isAuthenticated($clientKeysData->client_key);
+        $result = (new ClientHelpers())->isAuthenticated($clientKeysData['client_key']);
         $this->assertTrue(true);
         
     }
@@ -68,19 +66,10 @@ class MigrationTest extends TestCase
             "is_blocked" => "0",
         ];
 
-        $clientLastID = (new dbConnection())->insertIntoTable("clients", $clientData);
+        $lastID = (new dbConnection())->insertIntoTable("clients", $clientData);
         $client = (new dbConnection())->selectById($table, $lastID);
 
-        $clientKeysData = [
-            "name" =>  "TestAdmin3",
-            "client_id" => $client->id,
-            "client_key" => 'pat_privkey_jdhfolfko798',
-            "is_blocked" => "0",
-        ];
-
-        (new dbConnection())->insertIntoTable("client_keys", $data);
-
-        $result = (new ClientHelpers())->getClient("id", $client->id);
+        $result = (new ClientHelpers())->getClient("id", $client['id']);
         $this->assertTrue(true);
         
     }
